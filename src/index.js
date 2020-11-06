eabcd20b2aabd39a87cf7d18e74e44a870891808
Tomasz Fijoł <tomasz.fijol@bold.com>
2020-11-06 16:14:42 +0100

    0.0.0-beta.10

--- src/index.js
+++ src/index.js
@@ -21,7 +21,9 @@ class GraphQLClientError extends Error {
     try {
       message = response.error // string
         || response.errors && response.errors[0].message // object
-        || Array.isArray(response.result) && response.result.find(({ errors }) => errors.length).errors[0].message; // array
+        || Array.isArray(response.result) && response.result.find(({
+          errors,
+        }) => errors.length).errors[0].message; // array
     } catch (err) {
       message = `status ${response.status}`;
     }
@@ -41,7 +43,9 @@ async function getBody(response) {
   return response.text();
 }
 
-const checkResults = ({ data, errors }) => data && !errors;
+const checkResults = ({
+  data, errors,
+}) => data && !errors;
 
 function headersJSON(obj) {
   return new Headers({
@@ -59,6 +63,7 @@ export default async function gqlite(
     variables,
     body,
     method = 'post',
+    operationName,
     errorRequestVariables = false,
     headers,
     rawRequest,
@@ -68,19 +73,29 @@ export default async function gqlite(
   const response = await fetch(url, {
     method,
     headers: headersJSON(headers),
-    body: JSON.stringify(body || { query, variables }),
+    body: JSON.stringify(body || {
+      query,
+      variables,
+      operationName,
+    }),
     ...rest,
   });
 
   const result = await getBody(response);
-  const { headers: responseHeaders, status } = response;
+  const {
+    headers: responseHeaders, status,
+  } = response;
 
   if (response.ok && (checkResults(result) || result.length && result.every(checkResults))) {
     if (!rawRequest) {
       return result.data;
     }
 
-    return { result, status, headers: responseHeaders };
+    return {
+      result,
+      status,
+      headers: responseHeaders,
+    };
   }
 
   const resultType = Array.isArray(result) ? 'array' : typeof result;
@@ -88,18 +103,31 @@ export default async function gqlite(
 
   switch (resultType) {
     case 'string':
-      errorResult = { error: result };
+      errorResult = {
+        error: result,
+      };
       break;
     case 'array':
       // batch request (array of query and variables objects)
-      errorResult = { result };
+      errorResult = {
+        result,
+      };
       break;
     default:
       errorResult = result;
   }
 
   throw new GraphQLClientError(
-    { ...errorResult, headers: responseHeaders, status },
-    { body, query, variables: errorRequestVariables ? variables : noVariables },
+    {
+      ...errorResult,
+      headers: responseHeaders,
+      status,
+    },
+    {
+      body,
+      query,
+      variables: errorRequestVariables ? variables : noVariables,
+      operationName,
+    },
   );
 }
